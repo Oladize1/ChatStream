0b439400a7aa06220d51dfe9076ab1a43e599d4c
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-05-09 14:06:27 +0100

    Fix: error removed exposed  token from VideoCall component

--- client/src/Components/VideoCall.jsx
+++ client/src/Components/VideoCall.jsx
@@ -6,10 +6,6 @@ import { useAuthStore } from '../Store/Store';
 
 const VideoCall = () => {
     const { selectedUser } = useAuthStore()
-    const apiKey = 'zumsgar8e3t5';
-    const token ='rgv4zwjpqpyxexvcrg8mhxjsken7jbmwqx29matgkbfmm5fsk6u9x8j5qg9v46bf';
-    const userId = selectedUser;
-    const callId = 'FJ3NDdmA4XH3';
     console.log("selected user",selectedUser)
   return (
     <div>VideoCall</div>
